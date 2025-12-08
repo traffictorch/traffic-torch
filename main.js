ac1a3380cfe26c68827bf048983d6e885bc412c3
YliaCallan <traffictorch@gmail.com>
2025-12-09 07:16:55 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-  <!-- Scripts -->
-
     const menuToggle = document.getElementById('menuToggle');
     const mobileMenu = document.getElementById('mobileMenu');
     menuToggle.addEventListener('click', () => mobileMenu.classList.toggle('hidden'));
