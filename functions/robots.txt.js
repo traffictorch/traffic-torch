44a0b0f11b9a930245c0b93b02f18abdf6fa0766
YliaCallan <traffictorch@gmail.com>
2026-01-18 14:16:21 +1100

    Update robots.txt.js

--- functions/robots.txt.js
+++ functions/robots.txt.js
@@ -1,10 +1,13 @@
-export const onRequest = async () => {
-  return new Response(`User-agent: *
+export const onRequest = () => {
+  const customRobots = `User-agent: *
 Allow: /
 
-Sitemap: https://traffictorch.net/sitemap.xml`, {
+Sitemap: https://traffictorch.net/sitemap.xml`;
+
+  return new Response(customRobots, {
     headers: {
-      'Content-Type': 'text/plain'
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'public, max-age=3600'  // cache for 1 hour
     }
   });
 };
