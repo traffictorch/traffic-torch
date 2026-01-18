2c9b2c606e1dd506c1bd42bcc11f5a9ad7ad8724
YliaCallan <traffictorch@gmail.com>
2026-01-18 17:48:11 +1100

    Update script.js

--- seo-ux-tool/script.js
+++ seo-ux-tool/script.js
@@ -567,7 +567,7 @@ if (failedMetrics.length > 0) {
 
 const targetY = resultsWrapper.getBoundingClientRect().top + window.pageYOffset;
 const currentY = window.pageYOffset;
-const maxMove = 125;
+const maxMove = 165;
 
 window.scrollTo({
   top: Math.min(targetY, currentY + maxMove),
