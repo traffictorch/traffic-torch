a780fe3f6cb8c6e3e5a35f04a44592bfdf5438f7
YliaCallan <traffictorch@gmail.com>
2026-01-21 11:16:06 +1100

    Revert to minimal one-line Tailwind input - separate output - link last - no custom rules or conflicts

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./**/*.html', './src/**/*.{js,css}'],
-  darkMode: 'class',
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
